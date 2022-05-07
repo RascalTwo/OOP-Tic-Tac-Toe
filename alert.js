19dc427e720182aaaa341ec9ffeaa3e1dece3eb5
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-07 19:38:14 -0400

    Refactor Alert

--- alert.js
+++ alert.js
@@ -1,13 +1,13 @@
 class PromptTicTacToe extends TicTacToe {
 	constructor() {
 		super();
-		this.render();
+		this.play()
 	}
 
 	toString() {
 		const side = this._board.length / 3
 		const rows = [
-			['-', ...Array.from({ length: side }, (_, i) => i)]
+			['-', ...Array.from({ length: side }, (_, i) => i), 'C']
 		];
 		let row = [0];
 		for (let i = 0; i < this._board.length; i++) {
@@ -18,11 +18,31 @@ class PromptTicTacToe extends TicTacToe {
 			row.push(this._board[i] || '-')
 		}
 		if (row.length === side + 1) rows.push(row);
+		rows.push(['R'])
 
 		return `Current Player: ${this.currentPlayer}\n\n` + rows.map(row => row.join(' | ')).join('\n');
 	}
 
-	getTileIndex(message){
+	play(){
+		while (this.winner === undefined) {
+			let message = 'Enter Row,Column of position to place';
+			while (typeof message === 'string') {
+				message = this.render(message);
+			}
+			if (message === null) return alert('Draw!');
+			else if (!this.place(message)) message = 'That tile is already full!';
+		}
+
+		alert(this.winner ? this.winner + ' won!' : 'Draw!');
+
+		if (!confirm('Play again?')) return;
+		this.restart();
+		return this.play();
+	}
+
+	render(message) {
+		super.render();
+
 		const response = prompt(this + '\n\n' + message);
 		if (!response) return null;
 
@@ -31,27 +51,6 @@ class PromptTicTacToe extends TicTacToe {
 		if (x < 0 || x > 2 || y < 0 || y > 2) return 'Numbers must be between 0 and 2';
 
 		return x * 3 + y;
-	}
-
-	render(message = 'Enter X,Y of position to place') {
-		super.render();
-
-		while (typeof	message === 'string'){
-			message = this.getTileIndex(message);
-		}
-		if (message === null) return alert('Draw!');
-
-		if (!this.place(message)) return this.render('That tile is already full!');
-
-
-		const winner = this.winner;
-		if (winner === undefined) return this.render();
-		alert(winner ? winner + ' won!' : 'Draw!');
-
-
-		if (!confirm('Play again?')) return;
-		this.restart();
-		return this.render();
 	}
 }
 
