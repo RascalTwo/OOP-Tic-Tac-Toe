97c856d5098ccac5658c270c3f4a9ca1a91414b8
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-07 15:16:31 -0400

    Make game playable

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const WINNING_INDEXES = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+
+	[0, 4, 8],
+	[6, 4, 2]
+]
+
 const buttons = [...document.querySelectorAll('button')];
 
 let currentPlayer = 'X';
@@ -7,7 +20,16 @@ let currentPlayer = 'X';
  * @param {MouseEvent} event
  */
 function handleButtonClick(event){
-	event.currentTarget.textContent = currentPlayer
+	event.currentTarget.textContent = currentPlayer;
+	event.currentTarget.disabled = true;
+	currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+	for (const indexes of WINNING_INDEXES){
+		const first = buttons[indexes[0]].textContent
+		if (first && indexes.every(index => buttons[index].textContent === first)){
+			return alert(first + ' won!');
+		}
+	}
 }
 
 buttons.forEach(button => {
