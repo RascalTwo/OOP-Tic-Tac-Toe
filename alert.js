24023ee5c03015c66c011c40fe462f96c64ac677
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-07 19:30:29 -0400

    Refactor Alert

--- alert.js
+++ alert.js
@@ -5,39 +5,51 @@ class PromptTicTacToe extends TicTacToe {
 	}
 
 	toString() {
+		const side = this._board.length / 3
 		const rows = [
-			['-', 0, 1, 2]
+			['-', ...Array.from({ length: side }, (_, i) => i)]
 		];
 		let row = [0];
 		for (let i = 0; i < this._board.length; i++) {
-			if (row.length === 4) {
+			if (row.length === side + 1) {
 				rows.push(row);
 				row = [rows.length - 1];
 			}
 			row.push(this._board[i] || '-')
 		}
-		if (row.length === 4) rows.push(row);
+		if (row.length === side + 1) rows.push(row);
 
 		return `Current Player: ${this.currentPlayer}\n\n` + rows.map(row => row.join(' | ')).join('\n');
+	}
+
+	getTileIndex(message){
+		const response = prompt(this + '\n\n' + message);
+		if (!response) return null;
+
+		const [x, y] = response.split(',').map(part => parseInt(part))
+		if (isNaN(x) || isNaN(y)) return 'Must be numbers';
+		if (x < 0 || x > 2 || y < 0 || y > 2) return 'Numbers must be between 0 and 2';
+
+		return x * 3 + y;
 	}
 
 	render(message = 'Enter X,Y of position to place') {
 		super.render();
 
-		const response = prompt(this + '\n\n' + message);
-		if (!response) return alert('Draw!');
+		while (typeof	message === 'string'){
+			message = this.getTileIndex(message);
+		}
+		if (message === null) return alert('Draw!');
 
-		const [x, y] = response.split(',').map(part => parseInt(part))
-		if (isNaN(x) || isNaN(y)) return this.render('Must be numbers');
-		if (x < 0 || x > 2 || y < 0 || y > 2) return this.render('Numbers must be between 0 and 2');
-		if (!this.place(x * 3 + y)) return this.render('That tile is already full!');
+		if (!this.place(message)) return this.render('That tile is already full!');
 
 
 		const winner = this.winner;
 		if (winner === undefined) return this.render();
 		alert(winner ? winner + ' won!' : 'Draw!');
-		if (!confirm('Play again?')) return
 
+
+		if (!confirm('Play again?')) return;
 		this.restart();
 		return this.render();
 	}
